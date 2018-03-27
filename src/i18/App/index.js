3b58840ea5ab629ccad78c628ec68146873181bb
Denys Sidorov <000scorpions000@gmail.com>
2018-03-27 23:23:35 +0300

    test i18n

--- src/i18/App/index.js
+++ src/i18/App/index.js
@@ -1,17 +1,15 @@
 import React from "react";
-import { Component } from "react";
+import {Component} from "react";
 
 //import SearchBar from "../containers/search_bar";
 //import WeatherList from "../containers/weather_list";
-import I18 from '../i18/App/index'
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        {/*<SearchBar />*/}
-        {/*<WeatherList />*/}
-        <I18/>
+        Hello world
       </div>
     );
   }
